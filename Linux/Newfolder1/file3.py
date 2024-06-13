d540a2fb3a7c1a427fa901c3734411fd9c669ef8
mounika <mounikapagilla6@gmail.com>
2024-06-13 21:21:46 +0530

    Swap code

---
+++ Linux/Newfolder1/file3.py
@@ -0,0 +1,17 @@
+#include <iostream.h>
+#include <conio.h>
+class swap
+{
+    int a,b,temp;
+    public;
+    void  swap()
+    {
+        cout<<"Enter two numbers"<<;
+        cin>>a>>b;
+    }
+    void main()
+    temp=b;
+    b=a;
+    a=temp;
+    return 0;
+}
