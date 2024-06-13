6b70de90528defc1980431c115801bb18d90d511
mounika <mounikapagilla6@gmail.com>
2024-06-13 21:43:33 +0530

    GIT Information

---
+++ Linux/Newfolder2/Newfolder3/file6.py
@@ -0,0 +1,5 @@
+GLOBAL INFORMATION TRACKER
+Git is a version control system used for tracking changes in computer files.
+It is a free and open-source version control system used to handle small to very large projects efficiently.
+Git is used to tracking changes in the source code,enabling multiple developers to work together on non-linear development.
+Linux Torvalds created Git in 2005 for the development of the LINUX KERNAL.
